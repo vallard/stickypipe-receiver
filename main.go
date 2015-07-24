cc909c91b8c75027e0038d827880da9b43e9f49c
Vallard Benincosa <vallard@benincosa.com>
2015-07-24 15:32:48 -0700

    pretty clean environment with deps

--- main.go
+++ main.go
@@ -1,30 +1,38 @@
+// This program provides a sample web service that implements a
+// RESTFul CRUD API against a MongoDB database.
 package main
 
 import (
-	"gotraining/13-http/api/routes"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+
+	"github.com/vallard/stickypipe-receiver/routes"
 )
 
+// init is called before main. We are using init to customize logging output.
 func init() {
 	log.SetFlags(log.LstdFlags | log.Lmicroseconds | log.Lshortfile)
 }
 
+// main is the entry point for the application.
 func main() {
 	log.Println("main : Started")
 
+	// Check the environment for a configured port value.
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
 	}
 
+	// Create this goroutine to run the web server.
 	go func() {
 		log.Println("listener : Started : Listening on: http://localhost:" + port)
 		http.ListenAndServe(":"+port, routes.API())
 	}()
 
+	// Listen for an interrupt signal from the OS.
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, os.Interrupt)
 	<-sigChan
